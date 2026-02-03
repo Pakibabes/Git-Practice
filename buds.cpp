e0ab7c57324c2fde752ef85bc13d3ed77b587565
Pakibabes <navalcommander8@gmail.com>
2026-02-03 17:39:33 +0800

    Budskie

--- buds.cpp
+++ buds.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Budskie" << endl;
+    cout << "Budskie" << endl; //asd
     for(int i = 0; i< 5; i++){
         cout << i << endl;
     }
-    
+    cout << "Budskie" << endl;
     return 0;
 }
