18a0a7e06f8baf9a16275495c9cc5bdb2e302ac6
Pakibabes <navalcommander8@gmail.com>
2026-02-04 09:22:47 +0800

    Trying to create the interpreter

--- buds.cpp
+++ buds.cpp
@@ -2,17 +2,14 @@
 #include "star_shapes.cpp"
 using namespace std;
 int main(){
-    cout << "Budskie" << endl; //asd
-    for(int i = 0; i< 5; i++){
-        cout << i << endl;
+    string start;
+    cin >> start;
+
+    if(start == "START"){
+        cout << "BEGIN" << endl;
+        
+    }else{
+        cout << "ERROR: 420 \"Not beggining with a Start\"" << endl;
     }
-    cout << "How old are you Budskie? ";
-    int age;
-    cin >> age;
-
-
-    cout << "you are " << age << " years old" << endl;
-
-    shapes(3);
     return 0;
 }
